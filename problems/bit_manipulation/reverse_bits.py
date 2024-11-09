ff615625259fa978333e9d402732ebc6125af3c2
GoodestUsername <ericdongcannn@hotmail.com>
2024-11-08 17:58:41 -0800

    added explanation for reverse bits

--- problems/bit_manipulation/reverse_bits.py
+++ problems/bit_manipulation/reverse_bits.py
@@ -30,3 +30,24 @@ class tests(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+# pop out each bit and put it in front
+# class Solution:
+#     def reverseBits(self, n: int) -> int:
+#         res = 0
+#         for i in range(32):
+#             bit = (n >> i) & 1
+#             res += bit << (31 - i)
+#         return res
+
+
+# optimal bit manip
+# class Solution:
+#     def reverseBits(self, n: int) -> int:
+#         res = n
+#         res = (res >> 16) | (res << 16) & 0xFFFFFFFF
+#         res = ((res & 0xFF00FF00) >> 8) | ((res & 0x00FF00FF) << 8)
+#         res = ((res & 0xF0F0F0F0) >> 4) | ((res & 0x0F0F0F0F) << 4)
+#         res = ((res & 0xCCCCCCCC) >> 2) | ((res & 0x33333333) << 2)
+#         res = ((res & 0xAAAAAAAA) >> 1) | ((res & 0x55555555) << 1)
+#         return res & 0xFFFFFFFF
